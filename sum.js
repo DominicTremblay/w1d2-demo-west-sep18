0fdac4f4492da162ad5e8405e2a474534969d015
Dominic Tremblay <dominictremblay24@gmail.com>
2023-09-19 14:15:09 -0400

    adding the steps to solve the problem

--- sum.js
+++ sum.js
@@ -1,1 +1,15 @@
 // Write a node program that takes in an unlimited number of command line arguments, goes through each and prints out the sum of them. If any argument is not a whole number, skip it. Do support negative numbers though. If any argument is not a number, output an error message. We need at least 2 arguments.
+
+// get the arguments
+// Edge case: We need at least 2 arguments.
+
+// create an accumulator for the sum
+
+// iterate throught the commande line args
+// input validation
+// Edge case: If any argument is not a whole number, skip it.
+// Edge case: If any argument is not a number, output an error message. 
+
+// Add each argument
+
+// print the sum
